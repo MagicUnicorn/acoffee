128f25d17ef29a32bf7d022422a2bdc348dd32f7
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-18 21:45:00 +0300

    Добавлены модели баз данных для product и category

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+# Модели для меню
 
-# Create your models here.
+
+class Category(models.Model):
+	# Модель дял продуктов
+	name = models.CharField(max_length=128)
+	image = models.ImageField()
+	timestamp_create = models.DateTimeField(default=timezone.now)
+	timestamp_update = models.DateTimeField(default=timezone.now)
+	description = models.CharField(max_length=512)
+	is_deleted = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	# Модель для категорий
+	name = models.CharField(max_length=128)
+	category = models.ForeignKey('Category', related_name='products', on_delete='Cascade')
+	image = models.ImageField()
+	timestamp_create = models.DateTimeField(default=timezone.now)
+	timestamp_update = models.DateTimeField(default=timezone.now)
+	description = models.CharField(max_length=512)
+	is_deleted = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
