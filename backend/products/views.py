f5c4e034a621cc526596a3d3df9cf2b6210589e1
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-18 21:44:20 +0300

    Добавлены views для product и category

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import routers, serializers, viewsets
+from .models import Product, Category
+from .serialaizers import ProductSerializer, CategorySerializer
 
-# Create your views here.
+# Serializers define the API representation.
+
+
+# ViewSets define the view behavior.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(is_deleted=False)
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.filter(is_deleted=False)
+    serializer_class = CategorySerializer
