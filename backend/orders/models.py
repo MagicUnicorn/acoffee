f6adadf542124375892235328a6035a5964b224e
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-25 22:34:12 +0300

    Добавлена модель заказ

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Order(models.Model):
+	user = models.ForeignKey(User, related_name='orders', on_delete='Cascade')
+	product = models.ManyToManyField('products.Product', related_name='products')
+	datetime_open = models.DateTimeField()
+	datetime_close = models.DateTimeField()
+
