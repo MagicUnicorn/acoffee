84b454b4dadf749ad7165fa1e90cee850f7d7871
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-25 22:33:42 +0300

    Добавлено модель Contact

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+	town = models.CharField(max_length=128)
+	address = models.CharField(max_length=256)
+	phone_town = models.CharField(max_length=7)
+	phone_mts = models.CharField(max_length=7)
+	phone_vel = models.CharField(max_length=7)
+	phone_life = models.CharField(max_length=7)
