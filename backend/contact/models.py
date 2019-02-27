7155be2a7e582869d730d05ead9b956ccf3ed30a
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-27 22:55:53 +0300

    Добавлено приложение Контакты

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -6,7 +6,10 @@ from django.db import models
 class Contact(models.Model):
 	town = models.CharField(max_length=128)
 	address = models.CharField(max_length=256)
-	phone_town = models.CharField(max_length=7)
-	phone_mts = models.CharField(max_length=7)
-	phone_vel = models.CharField(max_length=7)
-	phone_life = models.CharField(max_length=7)
+	phone_town = models.CharField(max_length=13)
+	phone_mts = models.CharField(max_length=13)
+	phone_vel = models.CharField(max_length=13)
+	phone_life = models.CharField(max_length=13)
+
+	def __str__(self):
+		return self.address
