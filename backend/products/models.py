879d40d63a80fd99d57367a9d9e12ea2bc5e4a13
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-27 22:58:33 +0300

    Добавлено поля для акционных товаров в модель Продукты

--- backend/products/models.py
+++ backend/products/models.py
@@ -21,12 +21,14 @@ class Product(models.Model):
 	name = models.CharField(max_length=128)
 	category = models.ForeignKey('Category', related_name='products', on_delete='Cascade')
 	price = models.FloatField(default=0.0)
+	price_sale = models.FloatField(default=0.0)
 	image = models.ImageField()
 	timestamp_create = models.DateTimeField(default=timezone.now)
 	timestamp_update = models.DateTimeField(default=timezone.now)
 	description = models.CharField(max_length=512)
 	is_deleted = models.BooleanField(default=False)
 	is_vegan = models.BooleanField(default=False)
+	is_sale = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.name
