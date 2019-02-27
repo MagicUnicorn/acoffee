cfba559a6de8a7f66f99124de4d98bbcb3d050e2
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-02-27 22:57:53 +0300

    Добавлено приложение Отзывы

--- backend/review/models.py
+++ backend/review/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+	# Модель для отзывов
+	title = models.CharField(max_length=126)
+	body = models.TextField(max_length=10000)
+	user = models.ForeignKey(User, related_name='review_user', on_delete='Cascade')
+	timestamp_create = models.DateTimeField(default=timezone.now)
+	is_deleted = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.title
+
--- backend/review/views.py
+++ backend/review/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Review
+from .serialaizers import ReviewSerializer
 
-# Create your views here.
+# Serializers define the API representation.
+
+
+# ViewSets define the view behavior.
+class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Review.objects.filter(is_deleted=False)
+    serializer_class = ReviewSerializer
+
+
