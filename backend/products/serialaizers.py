ffa347b282f2c9b6886a3479ce1614b9ba070318
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-03-12 00:31:03 +0300

    Добавлены фильтры для продуктов

--- backend/products/serialaizers.py
+++ backend/products/serialaizers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
+
 from .models import Product, Category
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        exclude = ('is_deleted', )
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         exclude = ('is_deleted', )
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
+        exclude = ('is_deleted', )
+
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,33 @@
-from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
+
 from .models import Product, Category
 from .serialaizers import ProductSerializer, CategorySerializer
 
-# Serializers define the API representation.
 
-
-# ViewSets define the view behavior.
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.filter(is_deleted=False)
     serializer_class = ProductSerializer
+
+    @list_route(methods=['get'])
+    def get_vegan(self, request, pk=None):
+        queryset = self.queryset.filter(is_vegan=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @list_route(methods=['get'])
+    def get_sale(self, request, pk=None):
+        self.queryset.filter(is_vegan=True)
+        queryset = self.queryset.filter(is_sale=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @list_route(methods=['get'])
+    def get_sale_vegan(self, request, pk=None):
+        queryset = self.queryset.filter(is_vegan=True, is_sale=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
