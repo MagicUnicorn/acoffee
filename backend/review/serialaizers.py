fc667093693995ae860ec219ec713cb08d56f0d1
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-03-25 02:14:17 +0300

    Изменение сериалайзеров продукта и заказов

--- backend/review/serialaizers.py
+++ backend/review/serialaizers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import Review
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Review
         exclude = ('is_deleted', )
