4bc8a66b05aeba69f60117bdd1b737d3208515bb
Anna Zhdanova <zhdanowwwa@gmail.com>
2019-03-25 23:00:48 +0300

    Файлы библиотек добавлены в гитигнор файл

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -1,2 +1,6 @@
 
-export let host = 'http://zhdanova.by:8000';
+export let host = 'http://localhost:8000';
+
+            // "host": "0.0.0.0",
+            // "port": 80,
+            // "disableHostCheck": true
